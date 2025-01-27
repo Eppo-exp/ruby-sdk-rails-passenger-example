4f43516948c21ddaa64f6af8613fba738ad1b62b
Felipe Lima <felipe.lima@gmail.com>
2025-01-27 16:23:36 -0300

    set eppo client to Rails.application.config

--- config/initializers/eppo.rb
+++ config/initializers/eppo.rb
@@ -5,13 +5,15 @@ config = EppoClient::Config.new(ENV["EPPO_SDK_KEY"], log_level: "info")
 if defined?(PhusionPassenger)
   PhusionPassenger.on_event(:starting_worker_process) do |forked|
     if forked
-      Rails.application.eppo_client = EppoClient::init(config)
+      EppoClient::init(config)
     end
   end
 elsif defined?(Spring)
   Spring.after_fork do
-    Rails.application.eppo_client = EppoClient::init(config)
+    EppoClient::init(config)
   end
 else
-  Rails.application.eppo_client = EppoClient::init(config)
+  EppoClient::init(config)
 end
+
+Rails.application.config.eppo_client = EppoClient::Client.instance
